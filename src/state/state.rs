7647c493250ec9b084c8214da37a3dacd604c521
Zhuchengyu04 <zcylce02@gmail.com>
2021-03-31 10:32:29 +0800

    add vc impl53

--- src/state/state.rs
+++ src/state/state.rs
@@ -436,8 +436,13 @@ impl<B: DB> State<B> {
         for t in threads {
             t.join().unwrap();
         }
-        let all_sub_commitment = format!("{}{}{}{}", sub_commitments_0,sub_commitments_1,sub_commitments_2,sub_commitments_3);
-        let state_commitment = Commitment::new(&all_prover_params, all_sub_commitment.as_ref()).unwrap();
+        let all_sub_commitment:Vector<String> = Vec::with_capacity(4);
+        all_sub_commitment.push(sub_commitments_0);
+        all_sub_commitment.push(sub_commitments_1);
+        all_sub_commitment.push(sub_commitments_2);
+        all_sub_commitment.push(sub_commitments_3);
+        // format!("{}{}{}{}", sub_commitments_0,sub_commitments_1,sub_commitments_2,sub_commitments_3);
+        let state_commitment = Commitment::new(&all_prover_params, &all_sub_commitment).unwrap();
 
         self.root = H256::from(0);
         let mut commitment_bytes: Vec<u8> = vec![];
