36bdaae618ab29caa7d97a3ed0690ca1b38b3a6f
Zhuchengyu04 <zcylce02@gmail.com>
2021-03-31 13:08:13 +0800

    add vc impl64

--- src/state/state.rs
+++ src/state/state.rs
@@ -396,12 +396,21 @@ impl<B: DB> State<B> {
         }
         let mut sub_commitments:Vec<String> = Vec::with_capacity(4);
         let (tx, rx) = mpsc::channel();
+        let tx1 = mpsc::Sender::clone(&tx);
+        let tx2 = mpsc::Sender::clone(&tx);
+        let tx3 = mpsc::Sender::clone(&tx);
+        let mut tx_vec =Vec::with_capacity(4);
+        tx_vec.push(tx);
+        tx_vec.push(tx1);
+        tx_vec.push(tx2);
+        tx_vec.push(tx3);
         let mut threads = vec![];
 
         for i in 0..(3) {
             // let maps = slice_map.clone();
             let sizes = (slice_map.get(&i).unwrap().len() as u32);
-            let sub_value = slice_map.get(&i).unwrap();
+            let sub_value = slice_map.clone().get(&i).unwrap();
+            let sends = tx_vec[i];
             let t = thread::spawn(move || {
                 // create_vc_commitment(
                 //     &format!("123456789012345678901234567890{}-{}", l.to_string(), i.to_string()),
@@ -415,7 +424,7 @@ impl<B: DB> State<B> {
                 let state_commitment = Commitment::new(&prover_params, &sub_value).unwrap();
                 let mut commitment_bytes: Vec<u8> = vec![];
                 state_commitment.serialize(&mut commitment_bytes, true);
-                tx.send((i,format!("{:?}", String::from_utf8(commitment_bytes)))).unwrap();
+                sends.send((i,format!("{:?}", String::from_utf8(commitment_bytes)))).unwrap();
                 
             });
             threads.push(t);
